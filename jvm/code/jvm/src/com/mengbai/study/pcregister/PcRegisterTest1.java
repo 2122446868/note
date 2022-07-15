dfb3fbbfe3f22a7386408ee848cffcacc4dba95d
梦白 <2122446868@qq.com>
2022-07-15 13:23:25 +0800

    update jvm note

---
+++ jvm/code/jvm/src/com/mengbai/study/pcregister/PcRegisterTest1.java
@@ -0,0 +1,21 @@
+package com.mengbai.study.pcregister;
+
+/**
+ * @ProjectName jvm
+ * @Package com.mengbai.study.pcregister
+ * @ClassName PcRegisterTest1
+ * @Author ZCC
+ * @Date 2022/07/14
+ * @Description 程序计数器
+ * @Version 1.0
+ */
+public class PcRegisterTest1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        String d ="stst";
+        System.out.println(c);
+
+    }
+}
