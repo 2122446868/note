13816d9a887044fc7eb3f608f2f03f0cb53315cc
梦白 <2122446868@qq.com>
2022-07-26 17:10:29 +0800

    添加图片

--- jvm/code/jvm/src/com/mengbai/study/stack/LocalVariableTest.java
+++ jvm/code/jvm/src/com/mengbai/study/stack/LocalVariableTest.java
@@ -1,10 +1,62 @@
-package mengbai.study.stack;/**
+package mengbai.study.stack;
+
+import java.util.Date;
+
+/**
  * @ProjectName jvm
- * @Package     mengbai.study.stack
- * @ClassName   LocalVariableTest
- * @Author      ZCC
- * @Date        2022/07/25
- * @Description TODO
- * @Version     1.0
- */public class LocalVariableTest {
+ * @Package mengbai.study.stack
+ * @ClassName LocalVariableTest
+ * @Author ZCC
+ * @Date 2022/07/25
+ * @Description 局部变量表
+ * @Version 1.0
+ */
+public class LocalVariableTest {
+    private Integer count = 1;
+
+    public static void main(String[] args) {
+        LocalVariableTest localVariableTest = new LocalVariableTest();
+        int num = 1;
+        localVariableTest.method1();
+        System.out.println("end:" + num);
+
+
+    }
+
+    public void method1() {
+        Date date = new Date();
+        String s = "atguigu";
+        System.out.println(s + "：" + date);
+
+    }
+
+    public void method2() {
+        System.out.println(this.count);
+
+    }
+
+    public static void method3() {
+        long l = 123L;
+        double d = 2.0;
+
+    }
+
+
+    /**
+     * @throws
+     * @title method4
+     * @description slot 重复利用
+     * @author zcc
+     * @date 2022/7/26 15:19
+     */
+    public void method4() {
+        int a = 0;
+        {
+            int b = 2;
+            a =b+a;
+
+        }
+        // C会重复利用B的槽位
+        int c = 3;
+    }
 }
